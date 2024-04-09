795475dc71365502ee53dbb479888e8843083023
NotShrirang <shrirangmahajan123@gmail.com>
2024-04-09 15:14:14 +0530

    fix: requirements.txt and add model caching

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 from numpy import asarray
 from matplotlib import pyplot
 import matplotlib.pyplot as plt
@@ -9,18 +9,21 @@ from numpy.random import randn
 st.title("Generative Adversarial Network (GAN) for MNIST Handwritten Digit Generation")
 st.write("This is a simple demo of a GAN trained on the MNIST dataset. The model was trained for 10000 epochs.")
 
-generate = st.button("Generate Random Image")
-if generate:
-    model = load_model('./models/generator_lr0002_e10000.h5')
 
+@st.cache_resource()
+def load_model(path):
+    model = tf.keras.models.load_model(path)
+    return model
+
+
+model = load_model('./models/generator_lr0002_e10000.h5')
+fig = plt.figure(figsize=(5, 5))
+
+generate = st.button("Generate Random Image")
+
+if generate:
     vector = randn(100)
     vector = vector.reshape(1, 100)
-
     X = model.predict(vector, verbose=0)
-
-    fig = plt.figure(figsize=(5, 5))
-
     plt.imshow(X[0, :, :, 0], cmap='gray_r')
-
-    st.pyplot(fig, clear_figure=True, use_container_width=True)
-
+    st.pyplot(fig, clear_figure=True)
